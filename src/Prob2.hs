6ce085899cb670ef160092839608c1612326acf7
soverysour <sebi.f@tuta.io>
2019-12-02 21:34:36 +0200

    Done with prob2 & proper prob1 (aka no bug).

--- src/Prob2.hs
+++ src/Prob2.hs
@@ -1,6 +1,7 @@
 module Prob2
   ( main1
   , solve1
+  , solve2
   ) where
 
 import           ClassyPrelude
@@ -10,7 +11,35 @@ import qualified Data.Text.Read                 as T
 import qualified Data.Vector                    as V
 
 main1 :: IO ()
-main1 = readFileUtf8 "input2.in" >>= solve1
+main1 = readFileUtf8 "input2.in" >>= solve2
+
+upperBound :: Int
+upperBound = 69
+
+possibleVals :: [(Int, Int)]
+possibleVals =
+  let vals = [0 .. upperBound]
+   in vals >>= flip fmap vals . (,)
+
+solve2 :: Text -> IO ()
+solve2 txt = do
+  let maybeInts = traverse T.decimal . filter (/= "") $ T.splitOn "," txt
+  case maybeInts of
+    Left _ -> print ("Bad input." :: Text)
+    Right vec ->
+      let vec' = V.fromList $ fst <$> vec
+          tryIt (one, two) acc =
+            if evalState (iterating 0) (vec' V.// [(1, one), (2, two)]) ==
+               Just 19690720
+              then Just (one, two)
+              else acc
+          result = foldr tryIt Nothing possibleVals
+       in case result of
+            Nothing -> print ("No solution found." :: Text)
+            Just (noun, verb) -> do
+              print noun
+              print verb
+              print $ 100 * noun + verb
 
 solve1 :: Text -> IO ()
 solve1 txt = do
@@ -36,7 +65,7 @@ updated f pos = do
   let lhs = vec V.!? (pos + 1) >>= (vec V.!?)
       rhs = vec V.!? (pos + 2) >>= (vec V.!?)
       val = fromMaybe 0 $ liftA2 f lhs rhs
-      out = vec V.!? (pos + 3) >>= (vec V.!?)
+      out = vec V.!? (pos + 3)
       out' = fromMaybe 0 out
   modify' (V.// [(out', val)])
   iterating $ pos + 4
